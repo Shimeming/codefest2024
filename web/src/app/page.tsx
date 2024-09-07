67ee9ade61906b646410733b75b741e7057391cc
shimeming <shimeming@gmail.com>
2024-09-07 11:05:56 +0800

    feat: add backend to the web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,31 @@
 import NavCards from '@/components/nav-cards';
+import { sql } from "@vercel/postgres";
 
-const Page = (): React.JSX.Element => {
+
+const Page = async () => {
+  const data = await sql<LatestInvoiceRaw>`
+    SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    ORDER BY invoices.date DESC
+    LIMIT 5`;
+
+  const latestInvoices = data.rows.map((invoice) => ({
+    ...invoice,
+    amount: invoice.amount,
+  }));
+
   return (
     <>
       <main className="pt-32">
         <NavCards />
+        <div>
+          {latestInvoices.map((invoice) => (
+            <div key={invoice.id}>
+              {invoice.id}
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
