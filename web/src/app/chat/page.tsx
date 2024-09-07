70b0a987cbf69a39a2a8b2f41a0155ffbbdace01
shimeming <shimeming@gmail.com>
2024-09-07 20:16:06 +0800

    feat: add chat home page

--- web/src/app/chat/page.tsx
+++ web/src/app/chat/page.tsx
@@ -1,6 +1,35 @@
+import ContactCard from "./contact-card";
+const contacts = [
+  {
+    id: '1234',
+    online: true,
+    name: "金大森",
+    lastMessage: "Hello"
+  },
+  {
+    id: '5678',
+    online: false,
+    name: "金小森",
+    lastMessage: "bye"
+  },
+  {
+    id: '9012',
+    online: false,
+    name: "金森",
+    lastMessage: "ouo"
+  }
+];
+
 const Page = () => {
   return (
-    <>chat</>
+    <main className="flex flex-col gap-2">
+      {contacts.map((contact) => (
+        <ContactCard 
+          key={contact.id}
+          {...contact}
+        />
+      ))}
+    </main>
   )
 }
 
