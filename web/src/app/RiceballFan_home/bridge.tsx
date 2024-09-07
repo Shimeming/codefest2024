0b81030fbc293083f2dd1b90f1eaf7cb7735cb26
brainbrian2000 <brainbrian2000@gmail.com>
2024-09-07 21:26:08 +0800

    Testing_of_flutterObj

--- web/src/app/RiceballFan_home/bridge.tsx
+++ web/src/app/RiceballFan_home/bridge.tsx
@@ -19,7 +19,7 @@ import { resolve } from "path";
 
 let info_state : boolean = false;
 let loc_state : boolean = false;
-let map_state : boolean   = false;
+let device_state : boolean   = false;
 export let status_print : string = "null";
 const messageListener = (event: MessageEvent) => {
   if(flutterObject!== "undefined") {
@@ -36,9 +36,9 @@ const messageListener = (event: MessageEvent) => {
       GotUserLoc = temp_str;
       loc_state = true;
     }
-    else if(temp_json.name === "launch_map"){
-      GotUserLaunchMap = temp_str;
-      map_state = true;
+    else if(temp_json.name === "deviceinfo"){
+      GotUserDevice = temp_str;
+      device_state = true;
     // }else if (temp_json.name === "phone_number"){
 
     }
@@ -49,11 +49,13 @@ const messageListener = (event: MessageEvent) => {
 }
 let getUserInfoCommand : string = '{  "name": "userinfo", "data": null }';
 let getUserLocCommand  : string = '{  "name": "location", "data": null }';
-let getUserLaunchMap   : string = '{ "name": "launch_map","data": "https://maps.app.goo.gl/sQKx4n3WctXuS5Bw8"}';
+let getUserLaunchMap   : string = '{  "name": "launch_map","data": "https://maps.app.goo.gl/sQKx4n3WctXuS5Bw8"}';
+let getUserDevInfoCommand: string = '{  "name" : "deviceinfo", "data": null}'
 // export let GotUserInfo : string = '{"name": "userinfo","data": {  "id": "7f3562f4-bb3f-4ec7-89b9-da3b4b5ff250",  "account": "wz7786",  "username": "Wesley",  "realName": "金大森",  "idNo": "A123456789",  "email": "ist83903@bcaoo.com",  "phoneNo": "0932166777",  "birthday": "1988/12/12",  "memberType": "personal",  "verifyLevel": "3",  "addresses": [    {      "zip3": 104,      "city": "臺北市",      "town": "中山區",      "village": "正得里",      "street": "吉林路",      "usageType": "0",      "seq": 1,      "priority": true    }  ],  "residentAddress": "臺北市中山區吉林路 69 號 4 樓",    "citizen": true,"nativePeople": false,"cityInternetUid": ""}}';
 export let GotUserInfo  : string = "null";
 export let GotUserLoc  : string = "null";
 export let GotUserLaunchMap : string = "null";
+export let GotUserDevice : string = "null";
 export function GetUserFromApp(){
   // flutterObject = window.flutterObject;
   // flutterObject 已存在，可以執行操作
@@ -66,17 +68,19 @@ export function GetUserFromApp(){
     }
     flutterObject.addEventListener("message",messageListener);
     flutterObject.postMessage(getUserInfoCommand);
-    flutterObject.postMessage(getUserLaunchMap);
+    // flutterObject.postMessage(getUserLaunchMap);
+    flutterObject.postMessage(getUserDevInfoCommand);
+    flutterObject.postMessage(getUserDevInfoCommand);
     flutterObject.postMessage(getUserLocCommand);
     console.log("wait for ok",1000);
-    if(info_state !== true || loc_state !== true || map_state !== true){
+    if(info_state !== true || loc_state !== true || device_state !== true){
       //wait for ok
       console.log("wait for ok",3000);
     }
   } else {
 
-    console.log("flutterObject 不存在，無法執行操作。");
-    GotUserLoc = "error-no-flutterObject QuQ lalala QQQ trying next time";
+    // console.log("flutterObject 不存在，無法執行操作。");
+    // GotUserLoc = "error-no-flutterObject QuQ lalala QQQ trying next time";
     return;
   }
 
--- web/src/app/RiceballFan_home/page.tsx
+++ web/src/app/RiceballFan_home/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { GetUserFromApp, GotUserInfo, GotUserLaunchMap, GotUserLoc , status_print} from "./bridge";
+import { GetUserFromApp, GotUserInfo, GotUserLaunchMap, GotUserLoc , GotUserDevice,status_print} from "./bridge";
 
 // function sendMessageToFlutter(message: string) {
 //   if (typeof window.flutterObject !== "undefined" && window.flutterObject) {
@@ -14,19 +14,22 @@ import { GetUserFromApp, GotUserInfo, GotUserLaunchMap, GotUserLoc , status_prin
 
 const Page = () => {
     console.log("Waiting for flutterObject load...",2000);
-    // setTimeout(()=>{},2000);
+    setTimeout(()=>{},2000);
     GetUserFromApp();
     return (
         <div>
-            <h1>Test Page</h1>
+            {/* <h1>User   Info</h1> */}
             {/* print GotUserInfo... on page */}
             <p>{GotUserInfo}</p>
+            {/* <h1>Device Info</h1> */}
             {/* print GotUserLaunchMap... on page */}
-            <p>{GotUserLaunchMap}</p>
+            {/* <p>{GotUserDevice}</p> */}
+
+            {/* <h1>User Location with lon and lat</h1> */}
             {/* print GotUserLoc... on page */}
             <p>{GotUserLoc}</p>
 
-            <p>{status_print}</p>
+            {/* <p>{status_print}</p> */}
 
         </div>
     );
